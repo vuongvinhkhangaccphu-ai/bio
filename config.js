701a3e7d2b9a7e3501100d8de575d4e943accc25
vuongvinhkhangaccphu-ai <vuongvinhkhangaccphu@gmail.com>
2026-01-25 23:18:26 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    profile: {
+        name: "Vương Vĩnh Khang",
+        nickname: "@khang0811",
+    },
+    // MỤC 1: CÁ NHÂN
+    personalLinks: [
+        { title: "Facebook", url: "https://www.facebook.com/vinh.khang.vuong.766136" },
+        { title: "Discord", url: "https://discord.com/users/1061202628572364840" },
+    ],
+    // MỤC 2: CỘNG ĐỒNG
+    communityLinks: [
+        { title: "My team on Discord", url: "https://discord.gg/vrAFkX5K" },
+        { title: "My team on Facebook", url: "https://m.me/j/Aba05BnAOU6J1YlG/" },
+        { title: "ZuraMC on Facebook", url: "https://m.me/j/AbalTr9OvjOoN7-j/" },
+        { title: "ZuraMC on Discord", url: "https://discord.gg/Y9F87pDgCz" }
+    ],
+    email: "vuongvinhkhang98@gmail.com"
+};
